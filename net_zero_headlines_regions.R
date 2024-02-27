c1ce5da69110c9fc6fbaaa69167607a3b81f4986
amcphillips <a.j.mcphillips@gmail.com>
2024-02-27 17:05:22 +0000

    Creating regional summaries

--- net_zero_headlines_regions.R
+++ net_zero_headlines_regions.R
@@ -14,17 +14,14 @@ clean_names(raw)
 
 geog_look_up <- read_csv("WD21_LAD21_CTY21_RGN21_CTRY21.csv") %>%
   select(LAD21CD, RGN21CD, RGN21NM)
-  distinct(geog_look_up)
-  clean_names(geog_look_up)
-#  dplyr::select(LAD21CD, RGN21CD) 
-#  dplyr::distinct()
-# dplyr::filter(RGN21CD %in% paste0("E1200000", 1:3))
+distinct(geog_look_up)
+clean_names(geog_look_up)
 
+# Need to find region boundaries
 lad_boundaries <- read_sf("lad21buc.geojson")
 clean_names(lad_boundaries)
 
-# Calculate how many LAs they are in
-
+# Calculate how many regions they are in
 data <- raw %>%
   select(Companynumber, Companyname, BestEstimateCurrentEmployees, 
          BestEstimateCurrentGVA, BestEstimateCurrentTurnover, Localauthoritycodes, Postcodes) %>%
@@ -42,39 +39,22 @@ data <- raw %>%
 # Split each LA into a variable of its own
 data <- data %>%  
   separate(Localauthoritycodes, into = paste0("LA", 1 : max(data$No_LAs, na.rm = TRUE)), sep = ",") %>%
-
-# Create a row for each business in each LA
+  
+  # Create a row for each business in each LA
   pivot_longer(cols = starts_with("LA"), names_to = NULL, values_to = "LAcode", values_drop_na = TRUE) %>%
-
-# Produce summary of how many companies in each LA, total turnover, total employees
+  
+  # Produce summary of how many companies in each LA, total turnover, total employees
   group_by(LAcode) %>%
   summarise(No_Companies = n(), GVA = sum(GVA_per_LA), Turnover = sum(turn_per_LA), Employees = sum(empl_per_LA), GVA_job = GVA/Employees)
 
-# Add geography lookups
+# Need to add data for geo data for mapping
+geog2 <- inner_join(data, geog_look_up, by = c("LAcode" = "LAD21CD"))
 
-geog <- inner_join(data, geog_look_up, by = c("LAcode" = "LAD21CD"))
-
-geog <- 
-  inner_join(geog, lad_boundaries, by = c("LAcode" = "LAD21CD")) %>%
-  distinct()
-
-northern_geog <- geog %>%
-  filter(RGN21CD %in% paste0("E1200000", 1:3))
-
+geog2 <- 
+  inner_join(geog2, lad_boundaries, by = c("LAcode" = "LAD21CD")) %>%
+  distinct() %>%
+  group_by(RGN21NM) %>%
+  summarise(sum(No_Companies), sum(GVA), sum(Turnover), sum(Employees), GVA_job = (sum(GVA)/sum(Employees)))
+#Output csv of results
 # Produce summary table of how many companies in each region and LA, total turnover, total employees
-write_excel_csv(geog[,c("LAD21NM", "RGN21NM", "No_Companies", "GVA", "Turnover", "Employees", "GVA_job")], "outputs/national_LAs.csv")
-# Produce sum of how many are in Northern regions
-write_excel_csv(northern_geog[,c("LAD21NM", "RGN21NM", "No_Companies", "GVA", "Turnover", "Employees", "GVA_job")], "outputs/northern_LAs.csv")
-
-
-#creating graphs
-
-ggplot(geog, aes(fill = GVA_job)) + aes(geometry = geometry) + geom_sf() + scale_fill_continuous(type = "viridis") +
- theme_void() #+
-
-ggplot(northern_geog, aes(fill = GVA_job)) + aes(geometry = geometry) + geom_sf() + scale_fill_continuous(type = "viridis") +
-  theme_void()
-# creates graphs/maps by RTIC
-# ggplot2::facet_wrap("RTIC")
-
-
+write_excel_csv(geog2, "outputs/regions.csv")
